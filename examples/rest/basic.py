beb023a396c8bb42c1d32a1f1ef06df6e0dad815
hypergonial <46067571+hypergonial@users.noreply.github.com>
2023-12-31 12:51:59 +0100

    Update examples

--- examples/rest/basic.py
+++ examples/rest/basic.py
@@ -8,7 +8,7 @@ client = arc.RESTClient(bot)
 @client.include
 @arc.slash_command(name="hi", description="Say hi to someone!")
 async def hi_slash(
-    ctx: arc.Context[arc.RESTClient],
+    ctx: arc.RESTContext,
     user: arc.Option[hikari.User, arc.UserParams(description="The user to say hi to.")]
 ) -> None:
     await ctx.respond(f"Hey {user.mention}!")
