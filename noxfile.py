a114c40c3d57096447389aa9e5d604b0e935f545
hypergonial <46067571+hypergonial@users.noreply.github.com>
2024-01-08 21:54:27 +0100

    Use pinned deps in nox sessions

--- noxfile.py
+++ noxfile.py
@@ -12,14 +12,14 @@ options.sessions = ["format_fix", "pyright", "pytest", "docs"]
 
 @nox.session()
 def format_fix(session: nox.Session) -> None:
-    session.install("-U", "ruff")
+    session.install("-U", "ruff", "-c", "dev_requirements.txt")
     session.run("python", "-m", "ruff", "format", *SCRIPT_PATHS)
     session.run("python", "-m", "ruff", *SCRIPT_PATHS, "--fix")
 
 
 @nox.session()
 def format(session: nox.Session) -> None:
-    session.install("-U", "ruff")
+    session.install("-U", "ruff", "-c", "dev_requirements.txt")
     session.run("python", "-m", "ruff", "format", *SCRIPT_PATHS, "--check")
     session.run("python", "-m", "ruff", *SCRIPT_PATHS)
 
@@ -27,31 +27,25 @@ def format(session: nox.Session) -> None:
 @nox.session()
 def pyright(session: nox.Session) -> None:
     session.install(".")
-    session.install("-U", "pyright")
+    session.install("-U", "pyright", "-c", "dev_requirements.txt")
     session.run("pyright", PATH_TO_PROJECT, EXAMPLES_PATH)
 
 
 @nox.session()
 def pytest(session: nox.Session) -> None:
     session.install(".[dev]")
-    session.install("-U", "pytest")
-    session.install("-U", "pytest-asyncio")
     session.run("pytest", "tests")
 
 
 @nox.session()
 def docs(session: nox.Session) -> None:
-    session.install("-Ur", "doc_requirements.txt")
-    session.install("-Ur", "requirements.txt")
-    session.install("-U", "black")  # Signature formatting
+    session.install("-r", "doc_requirements.txt", "-r", "requirements.txt")
     session.run("python", "-m", "mkdocs", "-q", "build")
 
 
 @nox.session()
 def servedocs(session: nox.Session) -> None:
-    session.install("-Ur", "doc_requirements.txt")
-    session.install("-Ur", "requirements.txt")
-    session.install("-U", "black")  # Signature formatting
+    session.install("-r", "doc_requirements.txt", "-r", "requirements.txt")
     session.run("python", "-m", "mkdocs", "serve")
 
 
